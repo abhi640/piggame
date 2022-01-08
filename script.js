eff1302f2b604e25ae27ff978abbc3f87d75daaa
abhi640 <abhi6340@gmail.com>
2022-01-08 19:33:48 +0530

    current score logic added

--- script.js
+++ script.js
@@ -7,11 +7,16 @@ const dice = document.querySelector('.dice');
 const newGame = document.querySelector('.btn--new');
 const rollDice = document.querySelector('.btn--roll');
 const hold = document.querySelector('.btn--hold');
+let activePlayer = 0;
+
+const playerScores = [0, 0];
 
 dice.classList.add('hidden');
 
 player1el.textContent = 0;
 player2el.textContent = 0;
+
+let currentScore = 0;
 
 // Rolling the dice
 
@@ -21,4 +26,16 @@ rollDice.addEventListener('click', function () {
 
   dice.src = diceFile;
   dice.classList.remove('hidden');
+
+  currentScore += diceValue;
+
+  if (diceValue != 1) {
+    document.querySelector('#current--0').textContent = currentScore;
+  } else {
+    currentScore = 0;
+    document.querySelector('#current--0').textContent = currentScore;
+    toggle('activePlayer');
+  }
 });
+
+// Add current Score
